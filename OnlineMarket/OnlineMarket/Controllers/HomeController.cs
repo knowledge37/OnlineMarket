9ea86b09fb0bc9a8584cb050f4def307b4323049
knowledge <knowledge@tshimologong.joburg>
2020-10-05 22:30:59 +0200

    shopping list

--- OnlineMarket/OnlineMarket/Controllers/HomeController.cs
+++ OnlineMarket/OnlineMarket/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OnlineMarket.Data;
 using OnlineMarket.Models;
 
 namespace OnlineMarket.Controllers
@@ -12,10 +13,11 @@ namespace OnlineMarket.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly NkTechSolutionContext _context;
+        public HomeController(ILogger<HomeController> logger, NkTechSolutionContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -43,6 +45,13 @@ namespace OnlineMarket.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult Shop()
+        {
+            var result = from p in _context.Products
+                         orderby p.Category
+                         select p;
+            return View(result.ToList());
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
